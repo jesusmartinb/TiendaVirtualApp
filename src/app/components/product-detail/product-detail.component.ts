9d75ec4e498622869ae4212dee4548b2f12f41ae
Jesús Martín Blanco <jesusmartinb64@gmail.com>
2024-05-23 19:27:44 +0200

    Se implementa y maqueta el componente product-detail

--- src/app/components/product-detail/product-detail.component.ts
+++ src/app/components/product-detail/product-detail.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../interfaces/product';
+import { ProductsService } from '../../services/products.service';
+import { ActivatedRoute, Params, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.scss'
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit {
 
+  public product: any = {};
+
+  constructor(
+    private productsService: ProductsService,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.productsService.getProductById(params['id']).subscribe({
+          next: (response: any) => this.product = response,
+          error: (error: any) => alert('No hemos podido traer el producto desde el servidor, intentalo más tarde.')
+        });
+      }
+    )
+
+  }
 }
