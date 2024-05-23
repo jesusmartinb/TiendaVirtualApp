b58e1df599b3510e26df7178501e5be013510e28
Jesús Martín Blanco <jesusmartinb64@gmail.com>
2024-05-23 17:39:33 +0200

    Se crean los diferentes componentes que van ha formar parte de la aplicación

---
+++ src/app/components/products-list-component/products-list-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products-list-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './products-list-component.component.html',
+  styleUrl: './products-list-component.component.scss'
+})
+export class ProductsListComponentComponent {
+
+}
