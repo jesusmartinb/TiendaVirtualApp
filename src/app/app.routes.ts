3d1be754e847f379bc9bf02e4282122ed2c6c724
Jesús Martín Blanco <jesusmartinb64@gmail.com>
2024-05-23 17:50:51 +0200

    Se crean las rutas de la aplicación en el archivo app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { ProductsListComponentComponent } from './components/products-list-component/products-list-component.component';
+import { CarritoComponent } from './components/carrito/carrito.component';
+import { EmpresaComponent } from './components/empresa/empresa.component';
+import { RutaNoEncontradaComponent } from './components/ruta-no-encontrada/ruta-no-encontrada.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'productos', component: ProductsListComponentComponent },
+  { path: 'productos-detalle/:id/:stock', loadComponent: () => import('./components/product-detail/product-detail.component').then(
+      (c) => c.ProductDetailComponent
+    ) },
+  { path: 'empresa', component: EmpresaComponent },
+  { path: 'carrito', component: CarritoComponent },
+  { path: '', redirectTo: '/productos', pathMatch: 'full' },
+  { path: '**', component: RutaNoEncontradaComponent }
+];
